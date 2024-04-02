fce7eba4aad02eb46ed67dc9416c422c7fc22acd
debu100 <debajitchakraborty96@gmail.com>
2024-04-02 10:14:16 +0530

    changes made in css and js file

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//? Selecting Elements
+
 const form = document.querySelector("form");
 
 const ip = document.querySelector("#ip");
@@ -12,52 +14,53 @@ const imgBox = document.querySelector(".images-box");
 
 const details = document.querySelector(".details");
 
+//? Fetching Data
+
 const getMeal = async (dishName) => {
-  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${dishName}`;
+  try {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${dishName}`;
 
-  h1.innerHTML = "Fetching Items";
+    h1.innerHTML = "Fetching Items";
 
-  const respObj = await fetch(url);
+    const respObj = await fetch(url);
 
-  const jsonVal = await respObj.json();
+    const jsonVal = await respObj.json();
 
-  const mealArr = jsonVal.meals;
+    const mealArr = jsonVal.meals;
 
-  console.log(mealArr);
+    let data = "";
 
-  let data = "";
+    if (mealArr === null) {
+      error.innerHTML = "Enter Correct Dish Name";
 
-  if (mealArr === null) {
-    error.innerHTML = "Enter Correct Dish Name";
+      imgBox.innerHTML = data;
 
-    imgBox.innerHTML = data;
+      h1.innerHTML = "";
+    } else if (mealArr.length !== 0) {
+      error.innerHTML = "";
 
-    h1.innerHTML = "";
-  } else if (mealArr.length !== 0) {
-    error.innerHTML = "";
+      h1.innerHTML = "Items Details";
 
-    h1.innerHTML = "Items Details";
+      mealArr.forEach((el) => {
+        const imgSrc = el.strMealThumb;
 
-    mealArr.forEach((el) => {
-      const imgSrc = el.strMealThumb;
+        const itemName = el.strMeal;
 
-      const itemName = el.strMeal;
+        const itemArea = el.strArea;
 
-      const itemArea = el.strArea;
+        const itemIng1 = el.strIngredient1;
 
-      const itemIng1 = el.strIngredient1;
+        const itemIng2 = el.strIngredient2;
 
-      const itemIng2 = el.strIngredient2;
+        const itemIng3 = el.strIngredient3;
 
-      const itemIng3 = el.strIngredient3;
+        const itemIng4 = el.strIngredient4;
 
-      const itemIng4 = el.strIngredient4;
+        const itemIng5 = el.strIngredient5;
 
-      const itemIng5 = el.strIngredient5;
+        const itemIns = el.strInstructions;
 
-      const itemIns = el.strInstructions;
-
-      data += `<div class="img-box">
+        data += `<div class="img-box">
         <img src=${imgSrc} alt="" />
         <div class="overlay">
         <h3>${itemName}</h3> 
@@ -73,12 +76,17 @@ const getMeal = async (dishName) => {
       </div>
       </div>`;
 
-      imgBox.innerHTML = data;
-    });
-  } else {
-    error.innerHTML = "Couldn't Fetch Data";
+        imgBox.innerHTML = data;
+      });
+    } else {
+      error.innerHTML = "Couldn't Fetch Data";
+    }
+  } catch (err) {
+    alert(`Can't Fetch Images, Try Again Later...`);
   }
 };
+
+//? Event On form submit
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
